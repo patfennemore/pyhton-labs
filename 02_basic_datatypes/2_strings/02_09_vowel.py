ce74e91a289e4b946c698d08474b8372f97d59ec
Mantap Apa <patfennemore@gmail.com>
2020-05-02 14:11:54 +0700

    Number of vowels in sentence (Challenge not complete)

--- 02_basic_datatypes/2_strings/02_09_vowel.py
+++ 02_basic_datatypes/2_strings/02_09_vowel.py
@@ -6,3 +6,12 @@ CHALLENGE: Can you change the script so that it counts the occurrence of each in
            in the string and print a count for each of them?
 
 '''
+
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+count = 0
+for i in sentence.lower():
+    if i in "aeiou":
+        count += 1
+print(str(count)+ " Vowels in sentence")
+
