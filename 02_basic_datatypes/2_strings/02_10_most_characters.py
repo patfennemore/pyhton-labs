5156d98fdc9e9887f18ebf8ca04c4fc289107104
Mantap Apa <patfennemore@gmail.com>
2020-05-02 14:14:04 +0700

    Print 3 strings & give word count

--- 02_basic_datatypes/2_strings/02_10_most_characters.py
+++ 02_basic_datatypes/2_strings/02_10_most_characters.py
@@ -11,3 +11,28 @@ CHALLENGE: Can you edit to script to print only the string with the most charact
            into the topic "Conditionals" to solve this challenge.
 
 '''
+
+sentence = input("Please enter 3 words with spaces in between: ")
+word_list = sentence.split()
+
+first_word = word_list[0]
+second_word = word_list[1]
+third_word = word_list[2]
+
+first_word_length = len(word_list[0])
+second_word_length = len(word_list[1])
+third_word_length = len(word_list[2])
+
+print(str(first_word_length) + ", " + str(first_word))
+print(str(second_word_length) + ", " + str(second_word))
+print(str(third_word_length) + ", " + str(third_word))
+
+maximum_char = max(len(w)for w in word_list)
+print("The longest word in the list has",maximum_char, "characters")
+
+if len(word_list) == maximum_char:
+       print(word_list)
+
+
+
+
