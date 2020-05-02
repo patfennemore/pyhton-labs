9411a37888d787743aa649957e9528a278c7214a
Mantap Apa <patfennemore@gmail.com>
2020-05-02 10:08:09 +0700

    Demonstration of int & float

--- 02_basic_datatypes/1_numbers/02_05_convert.py
+++ 02_basic_datatypes/1_numbers/02_05_convert.py
@@ -10,3 +10,27 @@ Demonstrate how to:
     Take note of what information is lost when some conversions take place.
 
 '''
+
+# 1)
+int_original = 5
+float_example = float(int_original)
+
+print(float_example)
+
+# 2)
+int_example = int(float_example)
+
+print(int_example)
+
+# 3)
+floor_division = float_example//int_example
+
+print(floor_division)
+
+# 4)
+first_user_input = float(input("Please enter any number: "))
+second_user_input = float(input("Please enter another number: "))
+
+multiplication_result = first_user_input*second_user_input
+
+print(multiplication_result)
