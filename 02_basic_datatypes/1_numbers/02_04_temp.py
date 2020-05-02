cd1638edc8628116bfdd953f26a913035c4b92a7
Mantap Apa <patfennemore@gmail.com>
2020-05-02 10:04:42 +0700

    Fahrenheit to Celsius conversion

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -10,3 +10,8 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 
 '''
+
+degrees_fahrenheit = float(input("Please enter a temperature in Fahrenheit: "))
+degrees_celsius = (degrees_fahrenheit-32)*(5/9)
+
+print((str(degrees_fahrenheit) + " degrees fahrenheit = ") + (str(degrees_celsius) + " degrees celsius"))
