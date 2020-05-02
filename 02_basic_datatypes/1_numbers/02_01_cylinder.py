fecf2f3a070f15a575144dad2bcd27609cd62417
Mantap Apa <patfennemore@gmail.com>
2020-05-02 09:43:34 +0700

    Calculated surface area and volume of a cylinder

--- 02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ 02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,15 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+
+import math
+
+radius = 3.14
+
+height = 5
+
+volume_cylinder = math.pi*radius**2*height
+print(volume_cylinder)
+
+surface_area_cylinder = (2*math.pi*radius*height) + (2*math.pi*radius**2)
+print(surface_area_cylinder)
