4f753fbac49a3e6e6dd585290c5c1c99419ab4c4
Mantap Apa <patfennemore@gmail.com>
2020-05-02 10:23:46 +0700

    Calculating future value of investment

--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,3 +7,13 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+input_investment_amount = float(input("Please enter investment amount: "))
+input_interest_rate = float(input("Please enter interest rate in percentage: "))
+input_investment_years = float(input("Please enter the numbers of years to invest: "))
+
+input_interest_rate_decimal = input_interest_rate/100
+
+future_value = (input_investment_amount*(1+input_interest_rate_decimal)**input_investment_years)
+
+print(future_value)
