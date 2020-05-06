585c476bb4ed7fb184acc3b9f48efbee07035b96
Mantap Apa <patfennemore@gmail.com>
2020-05-06 15:36:31 +0700

    Finding unique numbers in list

--- 03_more_datatypes/2_lists/03_10_unique.py
+++ 03_more_datatypes/2_lists/03_10_unique.py
@@ -6,3 +6,13 @@ unique_list = [55, 'hi', 4, 13]
 
 
 '''
+
+original_list = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
+# count how many times an item in the list appears
+unique_list = []
+
+for obj in original_list:
+    if original_list.count(obj) == 1:
+        unique_list.append(obj)
+
+print("unique_list = " + str(unique_list))
