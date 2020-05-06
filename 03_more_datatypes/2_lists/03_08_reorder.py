ff18bcf43cfdfa2001bf5750bcd020bcb600f2a9
Mantap Apa <patfennemore@gmail.com>
2020-05-06 13:20:57 +0700

    Positions of user inputed numbers

--- 03_more_datatypes/2_lists/03_08_reorder.py
+++ 03_more_datatypes/2_lists/03_08_reorder.py
@@ -7,3 +7,22 @@ Example input:  1,2,3,4,5,6,7,8,9,10
 Example output: 2,4,6,8,10,9,7,5,3,1
 
 '''
+
+# user input 10 numbers
+user_input = input("Enter 10 numbers: ")
+
+# split number into list
+user_input_list = user_input.split()
+# set start index to 0
+position = -1
+# increase position by 2 every loop
+for num in user_input_list:
+    if position <= 7:
+        position += 2
+        print(user_input_list[position])
+        continue
+position = 0
+for num in user_input_list:
+    if position >= -8:
+        position -= 2
+        print(user_input_list[position])
