db41bba6fadb659e09d877b074fd2d0ba76f2e77
Mantap Apa <patfennemore@gmail.com>
2020-05-06 14:38:45 +0700

    Combining lists within a list

--- 03_more_datatypes/2_lists/03_09_flatten.py
+++ 03_more_datatypes/2_lists/03_09_flatten.py
@@ -7,3 +7,13 @@ flattened_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 '''
 
 starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+
+flattened_list = []
+x = -1
+
+for l in starting_list:
+    if x < 3:
+        x += 1
+        flattened_list.extend(starting_list[x])
+
+print("flattened_list = " +str(flattened_list))
