6c564ce565665e58e7ef17507a5890a4e8b046aa
Mantap Apa <patfennemore@gmail.com>
2020-05-07 12:21:35 +0700

    Creating a lidt of tuples from User

--- 03_more_datatypes/3_tuples/03_14_list_of_tuples.py
+++ 03_more_datatypes/3_tuples/03_14_list_of_tuples.py
@@ -6,3 +6,18 @@ input = "hello world"
 result_list = [('h', 'e', 'l', 'l', 'o'), ('w', 'o', 'r', 'l', 'd')]
 
 '''
+user_input = input("Please type a sentence: ")
+user_words = list(user_input.split())
+
+count = 0
+new_list = []
+
+for word in user_words:
+    if count == 0:
+        new_list.append(tuple(word))
+    count += 1
+    if count >= 2:
+        new_list.append(tuple(word))
+        result_list = list(new_list)
+
+print(result_list)
