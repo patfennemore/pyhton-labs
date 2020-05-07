aad1a166163eba227a587539b0825732f6fff2f6
Mantap Apa <patfennemore@gmail.com>
2020-05-07 21:11:55 +0700

    Adding pairs of tuples to a list

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,3 +11,26 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
+
+user_numbers = input("Please enter a list of numbers:")
+user_numbers_list = list(user_numbers.split())
+user_numbers_int = [int(string) for string in user_numbers_list]
+user_numbers_sorted = sorted(user_numbers_int)
+print("Sorted Numbers: " + str(user_numbers_sorted))
+
+tuple_list = []
+
+count = 0
+count_2 = 2
+
+for number in user_numbers_sorted:
+    if count_2 <= len(user_numbers_sorted):
+        new_list = list(user_numbers_sorted[count:count_2])
+        count += 2
+        count_2 += 2
+        tuple_list.append(tuple(new_list))
+if len(user_numbers_sorted) % 2 == 1:
+    new_list_1 = list(user_numbers_sorted[-1:])
+    new_list_1.append(0)
+    tuple_list.append(tuple(new_list_1))
+print(tuple_list)
