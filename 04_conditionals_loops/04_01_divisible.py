31358ec2e68616e23abbb3cb256b015d52d3c20e
Mantap Apa <patfennemore@gmail.com>
2020-05-08 19:01:14 +0700

    Using modula to determine if number is divisible by 3

--- 04_conditionals_loops/04_01_divisible.py
+++ 04_conditionals_loops/04_01_divisible.py
@@ -4,3 +4,8 @@ from the user and determines whether it is divisible by 3 using an if statement.
 Print the result.
 
 '''
+user_input = int(input("Choose a number between 1 and 1,000,000,000: "))
+if user_input % 3 == 0:
+    print(str(user_input) + " is divisible by 3")
+else:
+    print(str(user_input) + " is not divisible by 3")
