1d6a3faf0a49f04b06ba58eb693d82e3377cd6d0
Mantap Apa <patfennemore@gmail.com>
2020-05-09 06:52:20 +0700

    Using while loop to find every 5th number

--- 04_conditionals_loops/04_06_while.py
+++ 04_conditionals_loops/04_06_while.py
@@ -2,3 +2,11 @@
 Use a "while" loop to print out every fifth number counting from 1 to 1000.
 
 '''
+
+count = 1
+
+for number in range(1, 1001):
+    while count == number:
+        count += 1
+        if number % 5 == 0:
+            print(number)
