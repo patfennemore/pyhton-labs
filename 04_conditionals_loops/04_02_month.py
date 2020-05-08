c5f6f40f4b25c4f9bdfdde4753685b5093e3b986
Mantap Apa <patfennemore@gmail.com>
2020-05-08 20:23:10 +0700

    Finding a month related to number

--- 04_conditionals_loops/04_02_month.py
+++ 04_conditionals_loops/04_02_month.py
@@ -4,3 +4,18 @@ Take in a number from the user and print "January", "February", ...
 or other respectively. Use a "nested-if" statement.
 
 '''
+user_input = int(input("Please enter a number: "))
+month_dict = {}
+count = 0
+
+import calendar
+for month in range(1, 13):
+    count += 1
+    month_dict[calendar.month_name[month]] = count
+
+if user_input <= 12:
+    for key, value in month_dict.items():
+        if value == user_input:
+            print(key)
+else:
+    print("Other")
