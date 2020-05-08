1cdb21878e79bfea8730400879f68c1872a96f81
Mantap Apa <patfennemore@gmail.com>
2020-05-08 08:51:31 +0700

    Count number of times a character occurs

--- 03_more_datatypes/4_dictionaries/03_18_occurrence.py
+++ 03_more_datatypes/4_dictionaries/03_18_occurrence.py
@@ -6,3 +6,15 @@ user_input = "hello"
 result = {"h": 1, "e": 1, "l": 2, "o": 1}
 
 '''
+
+user_input = input("Please enter a word: ")
+
+user_input_char = []
+my_dict = {}
+
+for char in user_input:
+    user_input_char.append(char)
+    my_dict[char] = user_input_char.count(char)
+del my_dict[" "]
+
+print("result = " + str(my_dict))
