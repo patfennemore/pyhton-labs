4ec8fea98fffd792686fc8717ccd9ea11596af95
Mantap Apa <patfennemore@gmail.com>
2020-05-08 18:30:01 +0700

    Unneccesary use of list - corrected

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -20,16 +20,17 @@ print("Sorted Numbers: " + str(user_numbers_sorted))
 
 tuple_list = []
 
+size = len(user_numbers_sorted)
 count = 0
 count_2 = 2
 
 for number in user_numbers_sorted:
-    if count_2 <= len(user_numbers_sorted):
-        new_list = list(user_numbers_sorted[count:count_2])
+    if count_2 <= size:
+        new_list = tuple(user_numbers_sorted[count:count_2])
         count += 2
         count_2 += 2
-        tuple_list.append(tuple(new_list))
-if len(user_numbers_sorted) % 2 == 1:
+        tuple_list.append(new_list)
+if size % 2 == 1:
     new_list_1 = list(user_numbers_sorted[-1:])
     new_list_1.append(0)
     tuple_list.append(tuple(new_list_1))
