15b0173ff09dc50f48da1603fbd621ff4429e7ac
Mantap Apa <patfennemore@gmail.com>
2020-05-09 06:22:31 +0700

    Finding prime numbers in range

--- 04_conditionals_loops/04_04_prime.py
+++ 04_conditionals_loops/04_04_prime.py
@@ -2,3 +2,11 @@
 Print out every prime number between 1 and 100.
 
 '''
+
+for number in range(1, 101):
+    if number > 1:
+        for itself in range(2, number):
+            if (number % itself) == 0:
+                break
+        else:
+            print(number)
