b4096c7c8d513908807de28a8f8fa93c67cd8d44
Mantap Apa <patfennemore@gmail.com>
2020-05-08 08:22:49 +0700

    Create Dictionary and add to it using for loop

--- 03_more_datatypes/4_dictionaries/03_17_first_dict.py
+++ 03_more_datatypes/4_dictionaries/03_17_first_dict.py
@@ -4,3 +4,13 @@ Write a script that creates a dictionary of keys, n and values n*n for numbers 1
 result = {1: 1, 2: 4, 3: 9, ...and so on}
 
 '''
+
+my_dict = {}
+
+n = 0
+
+for n in range(0, 10):
+    n += 1
+    my_dict[n] = n*n
+
+print(my_dict)
