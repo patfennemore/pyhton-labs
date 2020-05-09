19fdf3322761f22c6e3a84630a4763e8f58a2308
Mantap Apa <patfennemore@gmail.com>
2020-05-09 07:59:27 +0700

    Making a table

--- 04_conditionals_loops/04_09_table.py
+++ 04_conditionals_loops/04_09_table.py
@@ -8,3 +8,16 @@ Use a loop to print the following table to the console:
  40 41 42 43 44 45 46 47 48 49
 
 '''
+
+count = -1
+
+row_numbers = []
+
+for number in range(0, 51):
+    count += 1
+    if count % 10 != 0:
+        row_numbers.append(number)
+    if count % 10 == 0:
+        print(*row_numbers, " ")
+        row_numbers.clear()
+        row_numbers.append(number)
