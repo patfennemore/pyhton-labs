4666122d631628f499c3548c47be0b3c7a9bc454
Mantap Apa <patfennemore@gmail.com>
2020-05-12 18:31:27 +0700

    Switching name around and using f string

--- 05_string_formatting/05_02_office_supplies.py
+++ 05_string_formatting/05_02_office_supplies.py
@@ -25,3 +25,31 @@ office = [
     {"full_name": "Creed Bratton", "item": "mung beans"},
     {"full_name": "Darryl Philbin", "item": "forklift"},
 ]
+
+list_items = []
+names = []
+
+for library in office:
+    for key, value in library.items():
+        if key == "full_name":
+            names.append(value)
+        if key == "item":
+            list_items.append(value)
+
+list_name = []
+
+for name in names:
+    list_name.append(name.split())
+
+list_name_moved = []
+
+for n in list_name:
+    if len(n) == 2:
+        list_name_moved.append(n[1].upper() + ", " + n[0])
+    if len(n) == 3:
+        list_name_moved.append(n[2] + ", " + n[0] + " " + n[1])
+
+
+for count in range(0, len(list_items)):
+    print(f'{(list_name_moved[count])}{(24-(len(list_name_moved[count])))*(" "):>} {(list_items[count])}')
+
