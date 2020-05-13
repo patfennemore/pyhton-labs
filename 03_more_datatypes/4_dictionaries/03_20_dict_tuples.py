8aea7eb7a81c0a36fb63c2c0157ec62ce34d10ad
Mantap Apa <patfennemore@gmail.com>
2020-05-13 13:49:59 +0700

    Made more efficient with use of lambda

--- 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
@@ -7,14 +7,13 @@ result_list = [("item3", 1), ("item1", 5), ("item2", 6)]
 '''
 input_dict = {"item1": 5, "item2": 6, "item3": 1}
 
-sorted_values = sorted(input_dict.values())
+unsorted_list = []
 
-new_list = []
+for key, value in input_dict.items():
+    new_tuple = (key, value)
+    unsorted_list.append(new_tuple)
 
-for sorted_value in sorted_values:
-    for key, value in input_dict.items():
-        if sorted_value == value:
-            new_tuple = (key, value)
-            new_list.append(new_tuple)
+sorted_list = sorted(unsorted_list, key=lambda element: element[-1])
 
-print(new_list)
+print(sorted_list)
+print(unsorted_list)
