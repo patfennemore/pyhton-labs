d4f240c4374d81ab6541ade3b147816afce3e561
Mantap Apa <patfennemore@gmail.com>
2020-05-15 20:43:15 +0700

    3 Functions calling on eachother

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -3,3 +3,21 @@ Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
 
 '''
+
+
+def print_text(user_input):
+    print(user_input)
+    return user_input
+
+
+def multiply_text(user_input):
+    print_text(user_input*3)
+    return user_input
+
+
+def new_line(user_input):
+    multiply_text(user_input + "\n")
+    return user_input
+
+
+new_line("Hello World")
