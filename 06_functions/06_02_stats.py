785cda6ff94b8cb17c2e21ad5a529df15ae2b1dd
Mantap Apa <patfennemore@gmail.com>
2020-05-15 11:15:19 +0700

    Function that finds max,min,avergae and sum

--- 06_functions/06_02_stats.py
+++ 06_functions/06_02_stats.py
@@ -6,8 +6,17 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
 
-# call the function below here
+def stats(user_list):
+
+    max_found = max(user_list)
+    min_found = min(user_list)
+    sum_found = sum(user_list)
+    avg = sum_found / len(user_list)
+
+    user_list = max_found, min_found, sum_found, avg
+
+    return print(user_list)
+
+
+stats(example_list)
