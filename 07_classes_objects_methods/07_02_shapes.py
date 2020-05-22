f62f650c79778699be3a2098a6ee7cae21c66d90
Mantap Apa <patfennemore@gmail.com>
2020-05-22 11:11:45 +0700

    Calculating area and perimeter of circle and rectangle

--- 07_classes_objects_methods/07_02_shapes.py
+++ 07_classes_objects_methods/07_02_shapes.py
@@ -7,3 +7,42 @@ Write methods in the appropriate class so that you can calculate the area (of th
 perimeter (of the rectangle) and circumference of the circle.
 
 '''
+import math
+
+
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        area = self.width * self.length
+        print(area)
+
+    def perimeter(self):
+        perimeter = (2 * self.width) + (2 * self.length)
+        print(perimeter)
+
+
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        area = math.pi * (self.radius ** 2)
+        print(area)
+
+    def circumference(self):
+        circumference = 2 * (math.pi * self.radius)
+        print(circumference)
+
+
+rec = Rectangle(2, 3)
+rec.area()
+rec.perimeter()
+
+cir = Circle(5)
+cir.area()
+cir.circumference()
