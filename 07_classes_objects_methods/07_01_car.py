0ab6b098710dc10b42a84a3cd204d1162e4d943e
Mantap Apa <patfennemore@gmail.com>
2020-05-22 09:38:05 +0700

    Creating a car class and functions

--- 07_classes_objects_methods/07_01_car.py
+++ 07_classes_objects_methods/07_01_car.py
@@ -9,3 +9,23 @@ Create at least two different objects of this Car class and demonstrate
 changing the objects attributes.
 
 '''
+
+class Car:
+    def __init__(self, model, year, max_speed):
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+
+    def accelerate(self):
+         self.max_speed += 5
+
+    def details(self):
+        print(f"You car is a {self.model} which was made in {self.year} and has a max speed of {self.max_speed}")
+
+
+ford = Car("Fiesta", 1998, 150)
+nissan = Car("champ", 2002, 110)
+nissan.accelerate()
+nissan.details()
+nissan.accelerate()
+nissan.details()
