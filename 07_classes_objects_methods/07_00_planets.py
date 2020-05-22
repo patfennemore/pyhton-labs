1d86422a5b0d467f7fb22e4c734c21b022b7a442
Mantap Apa <patfennemore@gmail.com>
2020-05-22 09:23:09 +0700

    Creating a class and prints out info

--- 07_classes_objects_methods/07_00_planets.py
+++ 07_classes_objects_methods/07_00_planets.py
@@ -6,5 +6,16 @@ Use the appropriate dunder method to get informative output with print()
 
 '''
 
-class Planet():
-    pass
+
+class Planet:
+    def __init__(self, color, name, system):
+        self.name = name
+        self.color = color
+        self.system = system
+
+    def __str__(self):
+        return f"{self.name} is {self.color} and is in the {self.system}"
+
+
+mars = Planet("red", "Mars", "Solar System")
+print(mars)
