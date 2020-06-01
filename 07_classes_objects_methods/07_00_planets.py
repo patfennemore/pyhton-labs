7dc0a6c3d59ab88cd0e6ee4944ba052a2b79c73d
Mantap Apa <patfennemore@gmail.com>
2020-06-01 09:15:22 +0700

    Practice adding different variable

--- 07_classes_objects_methods/07_00_planets.py
+++ 07_classes_objects_methods/07_00_planets.py
@@ -16,6 +16,16 @@ class Planet:
     def __str__(self):
         return f"{self.name} is {self.color} and is in the {self.system}"
 
+    def color(self):
+        return f"{self.color}"
+
 
 mars = Planet("red", "Mars", "Solar System")
 print(mars)
+
+earth = Planet("blue", "Earth", "Solar System")
+print(earth)
+
+earth.color = "green"
+print(earth)
+
