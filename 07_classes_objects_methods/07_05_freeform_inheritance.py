9c5303a0b4f4835900a7df6bc8db77c6b0fa9c03
Mantap Apa <patfennemore@gmail.com>
2020-06-03 07:59:51 +0700

    Practicing with inheritance 3 classes deep

--- 07_classes_objects_methods/07_05_freeform_inheritance.py
+++ 07_classes_objects_methods/07_05_freeform_inheritance.py
@@ -16,3 +16,40 @@ your own for this exercise but if you are stuck, some ideas include:
 - A Restaurant superclass, with Gourmet and FastFood subclasses.
 
 '''
+
+
+class Vehicle:
+    def __init__(self, model, year, speed):
+        self.model = model
+        self.year = year
+        self.speed = speed
+
+    def __str__(self):
+        return f"{self.model} goes at {self.speed} and was made in {self.year}"
+
+
+class Truck(Vehicle):
+    def __init__(self, model, year, speed, weight):
+        Vehicle.__init__(self, model, year, speed)
+        self.weight = weight
+
+    def __str__(self):
+        return f"{Vehicle.__str__(self)} and weighs {self.weight}kg"
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, model, year, speed, cc):
+        Vehicle.__init__(self, model, year, speed)
+        self.cc = cc
+
+    def __str__(self):
+        return f"{Vehicle.__str__(self)} and has an engine with {self.cc}cc"
+
+
+class OffRoadMotorBike(Motorcycle):
+    def __init__(self, model, year, speed, cc, suspension):
+        Motorcycle.__init__(self, model, year, speed, cc)
+        self.suspension = suspension
+
+    def __str__(self):
+        return f"{Motorcyle.__str__(self)} with the {self.suspension}"
