d5958a68679e61f4808e83905252cd15f4d7d8be
Mantap Apa <patfennemore@gmail.com>
2020-06-10 09:19:05 +0700

    Finding all words above a certain length

--- 08_file_io/08_00_long_words.py
+++ 08_file_io/08_00_long_words.py
@@ -2,3 +2,12 @@
 Write a program that reads words.txt and prints only the words
 with more than 20 characters (not counting whitespace).
 '''
+
+twenty_char = []
+
+with open("words.txt", "r") as fin:
+    for word in fin:
+        if len(word) >= 21:
+            twenty_char.append(word.rstrip())
+
+print(twenty_char)
