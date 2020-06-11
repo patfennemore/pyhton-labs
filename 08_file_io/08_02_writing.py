d3c27af3c2caf8afa6d7414dffa1a153909b22dd
Mantap Apa <patfennemore@gmail.com>
2020-06-11 14:11:36 +0700

    Reversing a list of words

--- 08_file_io/08_02_writing.py
+++ 08_file_io/08_02_writing.py
@@ -2,3 +2,12 @@
 Write a script that reads in the contents of words.txt and writes the contents in reverse
 to a new file words_reverse.txt.
 '''
+
+words = []
+with open("words.txt", "r") as fin:
+    for word in fin:
+        words.append(word)
+words_ordered = sorted(words, reverse=True)
+
+with open("words_reverse.txt", "w") as fout:
+    fout.write("".join(words_ordered))
